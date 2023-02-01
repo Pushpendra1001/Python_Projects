6b48b633dc20680ae4efbb6ac1897fb61039cb91
Pushpendra baswal <89896630+Pushpendra1001@users.noreply.github.com>
2023-02-01 19:12:33 +0530

    Delete Send_message_by_time.py

--- Send_message_by_time.py
+++
@@ -1,8 +0,0 @@
-import pywhatkit as kt
-
-Your_NO = input("Enter Your Mobile Number add with country code like +91 : ")
-message = input("Enter Your message 😉 :")
-hr = int(input("Enter time in hr : "))
-min = int(input("Enter Miniutes : "))
-
-kt.sendwhatmsg(Your_NO,message , hr,min)
